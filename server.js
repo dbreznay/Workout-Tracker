bef54cdc91bc098b2738b3ce55b4425dcd0f3f42
dbreznay <60904436+dbreznay@users.noreply.github.com>
2020-09-23 21:40:51 -0500

    trying to deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(morgan('dev'));
 
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    process.env.MONGODB_URI || 'mongodb://localhost/workout_tracker',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
