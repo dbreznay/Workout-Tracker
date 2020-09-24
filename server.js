957f43ce3e1a342b6de02ad5f1dec8f9307c8c5d
dbreznay <60904436+dbreznay@users.noreply.github.com>
2020-09-23 21:29:58 -0500

    updated port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 
